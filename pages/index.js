3fe788414382570baaef5ce10670d6075879dd84
Ray Gerstner <rgersther@mweb.com>
2021-05-06 21:45:15 +0200

    Story Branch2

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
       </Head>
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">MWEB Home Page - Branch from dev!</a>
+          Welcome to <a href="https://nextjs.org">MWEB Home Page - Story Branch2!</a>
         </h1>
 
         <p><Navbar /></p>
