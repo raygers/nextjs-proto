e0eab73bdfcb8b931ca49e0ce0f159f45edf0680
Ray Gerstner <rgersther@mweb.com>
2021-05-06 21:23:51 +0200

    Test branch from dev

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
       </Head>
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">MWEB Home Page1!</a>
+          Welcome to <a href="https://nextjs.org">MWEB Home Page - Branch from dev!</a>
         </h1>
 
         <p><Navbar /></p>
