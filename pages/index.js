8496aec041c2368e584fcfcf5a21b32883144697
Ray Gerstner <rgersther@mweb.com>
2021-05-06 22:42:54 +0200

    Rebrand 2021

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
       </Head>
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">MWEB Home Page</a>
+          Welcome to <a href="https://nextjs.org">Rebrand 2021</a>
         </h1>
 
         <h3>Dev Preview</h3>
